4f16b676e639082b85719a7a6152d914ae75c5a2
Arvind <arvindsutharaasday@gmail.com>
2023-12-22 22:33:38 +0530

    integration of background changer

--- script.js
+++ script.js
@@ -2,50 +2,51 @@ const inputbutton = document.getElementsByTagName("button")[0];
 const inputbox = document.getElementsByClassName("inputbox")[0];
 const ul = document.querySelector("ul");
 
-const delParent = (event) => {
-    event.target.parentNode.remove();
-}
+
 const lineThrough = (event) => {
     event.target.classList.toggle("liner");
 }
+ul.querySelector("li").addEventListener("click", lineThrough);
 
-const valuelen = () => {
-    return inputbox.value.length;
-}
-
-const delbutton = () => {
-    const del = document.createElement("button");
-    del.classList.add("del");
-    del.appendChild(document.createTextNode("del"));
-    del.addEventListener("click", delParent);
-
-    return del;
+const delParent = (event) => {
+    event.target.parentNode.remove();
 }
 
 const clicker = () => {
-    if(valuelen() > 0) {
+    if(inputbox.value !== "") {
         const div = document.createElement("div");
         div.classList.add("wrapper");
         ul.appendChild(div);
-
+        
         const li = document.createElement("li");
         li.appendChild(document.createTextNode(inputbox.value));
         li.addEventListener("click", lineThrough);
         li.classList.add("theek");
         div.appendChild(li);
-
-        div.appendChild(delbutton());
+        
+        const del = document.createElement("button");
+        del.classList.add("del");
+        del.appendChild(document.createTextNode("del"));
+        del.addEventListener("click", delParent);
+        div.appendChild(del);
 
         inputbox.value = "";
     }
 }
+document.querySelector("button.del").addEventListener("click", delParent);
 
-const keypress = (event) => {
+inputbox.addEventListener("keydown", (event) => {
     if(event.key === "Enter") clicker();
-}
-
-inputbox.addEventListener("keydown", keypress);
+});
 inputbutton.addEventListener("click", clicker);
 
-document.querySelector("button.del").addEventListener("click", delParent);
-ul.querySelector("li").addEventListener("click", lineThrough);
+
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+
+const bgChanger = () => {
+    document.body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+}
+
+color1.addEventListener("input", bgChanger);
+color2.addEventListener("input", bgChanger);
